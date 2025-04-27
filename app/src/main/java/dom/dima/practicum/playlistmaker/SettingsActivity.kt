479c05d4d7aaa210c92cdb482674b0a5e745d37a
dmitriy <dchechumaev@mail.ru>
2025-04-28 01:29:39 +0300

    Исправление замечаний

--- app/src/main/java/dom/dima/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/dom/dima/practicum/playlistmaker/SettingsActivity.kt
@@ -1,19 +1,17 @@
 package dom.dima.practicum.playlistmaker
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val buttonBack = findViewById<ImageButton>(R.id.button_back)
-        buttonBack.setOnClickListener{
-            val displayIntent = Intent(this, MainActivity::class.java)
-            startActivity(displayIntent)
+        val viewSettings = findViewById<MaterialToolbar>(R.id.settings_layout)
+        viewSettings.setNavigationOnClickListener{
+            this.finish()
         }
     }
 }
