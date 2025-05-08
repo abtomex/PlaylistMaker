c9b01d27051ba8c63a9da3f45cf46669062f304b
dmitriy <dchechumaev@mail.ru>
2025-05-08 03:16:23 +0300

    Sprint_9. Homework

--- app/src/main/java/dom/dima/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/dom/dima/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,67 @@
 package dom.dima.practicum.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AbstractButtonBackActivity() {
+
+    var inputSearchText: String = DEFAULT_STR
+
+    companion object {
+        const val SAVED_TEXT = "SAVED_TEXT"
+        const val DEFAULT_STR = ""
+    }
+
+
+    override fun buttonBackId(): Int {
+        return R.id.search_layout
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButton.setOnClickListener {
+            searchEditText.text.clear()
+            searchEditText.clearFocus()
+            (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                .hideSoftInputFromWindow(searchEditText.windowToken, 0)
+        }
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!s.isNullOrEmpty()) {
+                    clearButton.visibility = View.VISIBLE
+                } else {
+                    clearButton.visibility = View.GONE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                inputSearchText = s.toString()
+            }
+
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SAVED_TEXT, inputSearchText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputSearchText = savedInstanceState.getString(SAVED_TEXT, DEFAULT_STR)
+        findViewById<EditText>(R.id.searchEditText).setText(inputSearchText)
     }
 }
--- app/src/main/java/dom/dima/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/dom/dima/practicum/playlistmaker/SettingsActivity.kt
@@ -1,17 +1,90 @@
 package dom.dima.practicum.playlistmaker
 
+import android.annotation.SuppressLint
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.appbar.MaterialToolbar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
+import androidx.core.net.toUri
+import androidx.core.content.edit
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AbstractButtonBackActivity() {
+
+    companion object {
+        const val SETTINGS = "settings"
+        const val DARK_MODE = "dark_mode"
+    }
+
+    override fun buttonBackId(): Int {
+        return R.id.settings_layout
+    }
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val viewSettings = findViewById<MaterialToolbar>(R.id.settings_layout)
-        viewSettings.setNavigationOnClickListener{
-            this.finish()
+        val switch = findViewById<SwitchCompat>(R.id.night_theme_switch)
+        val prefs = getSharedPreferences(SETTINGS, MODE_PRIVATE)
+        val isDark = prefs.getBoolean(DARK_MODE, false)
+
+        switch.isChecked = isDark
+        AppCompatDelegate.setDefaultNightMode(
+            if (isDark) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+        )
+
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            prefs.edit() { putBoolean(DARK_MODE, isChecked) }
+            AppCompatDelegate.setDefaultNightMode(
+                if (isChecked) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+            )
+            restartWithTheme()
+        }
+
+
+        val buttonShare = findViewById<TextView>(R.id.share_text)
+        buttonShare.setOnClickListener {
+
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                setType("text/plain")
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.android_course_url))
+            }
+
+            startActivity(shareIntent)
+
+        }
+
+        val buttonCheckSupport = findViewById<TextView>(R.id.helpdesk_text)
+        buttonCheckSupport.setOnClickListener {
+
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = "mailto:".toUri()
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.my_email)))
+            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+            putExtra(Intent.EXTRA_TEXT, getString(R.string.email_text))
+        }
+            startActivity(supportIntent)
+        }
+
+        val buttonAgreement = findViewById<TextView>(R.id.agreement_text)
+        buttonAgreement.setOnClickListener {
+            val practicumOffer = getString(R.string.practicum_offer)
+            val offerIntent = Intent(Intent.ACTION_VIEW)
+            offerIntent.data = practicumOffer.toUri()
+
+            startActivity(offerIntent)
         }
     }
+
+    private fun restartWithTheme() {
+        val intent = intent
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        val options = ActivityOptions.makeCustomAnimation(this, 0, 0)
+        startActivity(intent, options.toBundle())
+        finish()
+    }
+
+
 }
